698c8b857a0cc219d4c1937a648fa42ecad82f6c
minhbka <minhbka@gmail.com>
2021-06-30 20:51:08 +0900

    app basic

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,20 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(
-    Center(
-      child:Text( 
-      "Hello Flutter", 
-      textDirection: TextDirection.ltr
+    new MaterialApp(
+      title: "My Flutter App",
+      home: Scaffold(
+        appBar: AppBar(title: Text("My First App Screen"),),
+        body: Material(
+          color:Colors.lightBlueAccent,
+          child:Center(
+            child:Text( 
+              "Hello Flutter", 
+              textDirection: TextDirection.ltr,
+              style:TextStyle(color:Colors.white, fontSize:40.0),
+            )
+          )
+        )
       )
     )
   );
