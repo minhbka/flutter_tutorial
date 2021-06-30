8f6e11a1c22ff048044d390277c53d1fcb6f83d2
minhbka <minhbka@gmail.com>
2021-06-30 23:07:30 +0900

    Text Style in Flutter

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -1,23 +1,26 @@
 import 'package:flutter/material.dart';
 
-
-class Home extends StatelessWidget{
+class Home extends StatelessWidget {
   @override
   Widget build(Object context) {
     return Center(
-      child:Container(
-        alignment: Alignment.center,
-        color: Colors.deepPurple,
-        // width:200.0,
-        // height:100.0,
-        margin:EdgeInsets.only(left: 35.0),
-        
-        child:Text(
-          "Flight", 
-          textDirection: TextDirection.ltr,
-        )
-      )
-    );
-  }
+        child: Container(
+            alignment: Alignment.center,
+            color: Colors.deepPurple,
+            // width:200.0,
+            // height:100.0,
+            // margin:EdgeInsets.only(left: 35.0),
 
+            child: Text("Flight",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 50.0, 
+                    color:Colors.white,
+                    fontFamily: 'Roboto',
+                    fontWeight:FontWeight.w700,
+                    // fontStyle: FontStyle.italic,
+
+                  ))));
+  }
 }
