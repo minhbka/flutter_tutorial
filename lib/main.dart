85051d50615eee05c8510c2ea3081624eb8109c2
minhbka <minhbka@gmail.com>
2021-06-30 21:29:19 +0900

    code organization

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
+import './app_screens/first_creen.dart';
+void main()=>runApp(new MyFlutterApp());
+  
 
-void main(){
-  runApp(
-    new MaterialApp(
+
+class MyFlutterApp extends StatelessWidget{
+  @override
+  Widget build(Object context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "My Flutter App",
       home: Scaffold(
         appBar: AppBar(title: Text("My First App Screen"),),
-        body: Material(
-          color:Colors.lightBlueAccent,
-          child:Center(
-            child:Text( 
-              "Hello Flutter", 
-              textDirection: TextDirection.ltr,
-              style:TextStyle(color:Colors.white, fontSize:40.0),
-            )
-          )
-        )
+        body: FirstScreen()
       )
-    )
-  );
+    );
+  }
+  
 }
