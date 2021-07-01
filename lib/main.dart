8f37a0007e1760679faf056997751734e4bd7dc7
minhbka <minhbka@gmail.com>
2021-07-01 13:56:22 +0900

    create stateful widget app

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,51 @@
 import 'package:flutter/material.dart';
-import './app_screens/home.dart';
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: "Exploring UI widgets",
-    home: Scaffold(
-      appBar: AppBar(title: Text("Long List")),
-      body:getListView(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () =>debugPrint("FAB is clicked"),
-        child:Icon(Icons.add),
-        tooltip:'Add one more Item',
-      ),
-    ),
-  ));
-}
-
-void showSnackBar(BuildContext context, String item){
-  var snackBar = SnackBar(
-    content:Text("You just tapped $item"),
-    action:SnackBarAction(
-      label:"Undo",
-      onPressed:()=>debugPrint('Performing dummy Undo opration')
+void main(){
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Stateful App Example',
+      home: FavoriteCity(),
     )
   );
-
-  ScaffoldMessenger.of(context).showSnackBar(snackBar);
 }
 
-List<String> getListElements(){
-  var items = List<String>.generate(1000, (index) => "Item $index");
-  return items;
+class FavoriteCity extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _FavoriteCityState();
+  }
 }
 
+class _FavoriteCityState extends State<FavoriteCity>{
+  String nameCity ="";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stateful App Example')
+      ),
+      body: Container(
+        margin:EdgeInsets.all(20.0),
+        child:Column(
+          children:<Widget>[
+            TextField(
+              onSubmitted: (String userInput) => {
+                setState(()=> nameCity = userInput)
+              }
+            
+            ),
+            Padding(
+              padding:EdgeInsets.all(30.0),
+              child: Text(
+              "Your best city is " + nameCity,
+              style: TextStyle(fontSize: 20.0)
+            )
+            ),
+          ]
+        )
+      ),
+    );
 
-Widget getListView(){
-  var listItem = getListElements();
-  var listView = ListView.builder(
-    itemBuilder: (context, index){
-      return ListTile(
-        leading: Icon(Icons.arrow_right),
-        title:Text(listItem[index]),
-        onTap:()=>showSnackBar(context, listItem[index]),
-      );
-    }
-  );
-  return listView;
+  }
+
 }
