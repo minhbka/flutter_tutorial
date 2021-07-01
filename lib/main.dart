e2587bd550e22e0cb439361060fd738785086282
minhbka <minhbka@gmail.com>
2021-07-01 10:29:26 +0900

    Basic List View

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,42 @@ void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: "Exploring UI widgets",
-    home: Home(),
+    home: Scaffold(
+      appBar: AppBar(title: Text("Basic List View")),
+      body:getListView()
+    ),
   ));
+}
+
+Widget getListView(){
+  var listView = new ListView(
+    children:<Widget>[
+      ListTile(
+        leading:Icon(Icons.landscape),
+        title: Text("Landscape"),
+        subtitle: Text("Beautiful View!"),
+        trailing:Icon(Icons.wb_sunny),
+        onTap: ()=>debugPrint("Landscape tapped"),
+      ),
+      ListTile(
+        leading:Icon(Icons.landscape),
+        title: Text("Window"),
+        subtitle: Text("Beautiful View!"),
+        trailing:Icon(Icons.wb_sunny),
+      ),
+      ListTile(
+        leading:Icon(Icons.landscape),
+        title: Text("Phone"),
+        subtitle: Text("Beautiful View!"),
+        trailing:Icon(Icons.wb_sunny),
+      ),
+      Text("Another element in List"),
+      Container(
+        margin:EdgeInsets.only(top:30.0),
+        color:Colors.red,
+        height: 50.0
+      )
+    ]
+  );
+  return listView;
 }
