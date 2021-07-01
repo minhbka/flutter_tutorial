a68ab74ea2ced0d018dfd5b1dc10ec80220b3e94
minhbka <minhbka@gmail.com>
2021-07-01 09:39:25 +0900

    Row, Column and Expanded

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -4,23 +4,76 @@ class Home extends StatelessWidget {
   @override
   Widget build(Object context) {
     return Center(
-        child: Container(
-            alignment: Alignment.center,
-            color: Colors.deepPurple,
-            // width:200.0,
-            // height:100.0,
-            // margin:EdgeInsets.only(left: 35.0),
-
-            child: Text("Flight",
-                textDirection: TextDirection.ltr,
-                style: TextStyle(
-                    decoration: TextDecoration.none,
-                    fontSize: 50.0, 
-                    color:Colors.white,
-                    fontFamily: 'Roboto',
-                    fontWeight:FontWeight.w700,
+      child: Container(
+        padding:EdgeInsets.only(left:10.0, top:40.0),
+        alignment: Alignment.center,
+        color: Colors.deepPurple,
+        // width:200.0,
+        // height:100.0,
+        // margin:EdgeInsets.only(left: 35.0),
+        child: Column(
+          children:<Widget>[
+            Row(
+              children:<Widget>[
+                Expanded(child: Text("Jeju Air",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 35.0, 
+                      color:Colors.white,
+                      fontFamily: 'Roboto',
+                      fontWeight:FontWeight.w300,
+                      // fontStyle: FontStyle.italic,
+                    )
+                  )
+                ),
+                Expanded(child: Text(
+                    "From Seoul to Hanoi via HongKong",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 20.0, 
+                        color:Colors.white,
+                        fontFamily: 'Roboto',
+                        fontWeight:FontWeight.w300,
                     // fontStyle: FontStyle.italic,
-
-                  ))));
+                    )
+                  )
+                )
+              ]
+            ),
+            Row(
+              children:<Widget>[
+                Expanded(child: Text("Air Seoul",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 35.0, 
+                      color:Colors.white,
+                      fontFamily: 'Roboto',
+                      fontWeight:FontWeight.w300,
+                      // fontStyle: FontStyle.italic,
+                    )
+                  )
+                ),
+                Expanded(child: Text(
+                    "From Busan to Nha Trang",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 20.0, 
+                        color:Colors.white,
+                        fontFamily: 'Roboto',
+                        fontWeight:FontWeight.w300,
+                    // fontStyle: FontStyle.italic,
+                    )
+                  )
+                )
+              ]
+            )
+          ],
+        )
+      )
+    );
   }
 }
