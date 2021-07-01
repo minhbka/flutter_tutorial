87a2638895133eaf18a511c2b8ad844fd98b63d4
minhbka <minhbka@gmail.com>
2021-07-01 11:19:26 +0900

    Action Button ( FAB ) and Snack Bar

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,28 @@ void main() {
     title: "Exploring UI widgets",
     home: Scaffold(
       appBar: AppBar(title: Text("Long List")),
-      body:getListView()
+      body:getListView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () =>debugPrint("FAB is clicked"),
+        child:Icon(Icons.add),
+        tooltip:'Add one more Item',
+      ),
     ),
   ));
 }
+
+void showSnackBar(BuildContext context, String item){
+  var snackBar = SnackBar(
+    content:Text("You just tapped $item"),
+    action:SnackBarAction(
+      label:"Undo",
+      onPressed:()=>debugPrint('Performing dummy Undo opration')
+    )
+  );
+
+  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+}
+
 List<String> getListElements(){
   var items = List<String>.generate(1000, (index) => "Item $index");
   return items;
@@ -23,7 +41,7 @@ Widget getListView(){
       return ListTile(
         leading: Icon(Icons.arrow_right),
         title:Text(listItem[index]),
-        onTap:()=>debugPrint("${listItem[index]} was tapped"),
+        onTap:()=>showSnackBar(context, listItem[index]),
       );
     }
   );
